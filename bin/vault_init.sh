21e2ad70a4ed583fbdae85ac2a77142cd921c1ac
em <eugen.mayer@kontextwork.de>
2017-11-16 09:56:45 +0100

    add needed env vars to init_vars

--- bin/vault_init.sh
+++ bin/vault_init.sh
@@ -2,9 +2,9 @@
 # set -x
 set -e
 
-export DOCKER_IP=vault
 export VAULT_ADDR=https://vault:8200
 export VAULT_SKIP_VERIFY=true
+export VAULT_CACERT=/vault/server/server.crt
 
 echo "waiting for vault to start up.."
 wait-for-it -h vault -p 8200
@@ -12,6 +12,9 @@ echo "..vault up"
 
 vault init -check || true  # should return 'Vault is not initialized'
 vault init -ca-cert=${VAULT_SERVER_HOME}/server.crt -address=${VAULT_ADDR} -key-shares=1 -key-threshold=1 | tee -a ${VAULT_SERVER_HOME}/init_output | awk 'BEGIN{OFS=""} /Unseal/ {print "export VAULT_UNSEAL_KEY=",$4};/Root/ {print "export VAULT_ROOT_TOKEN=",$4}' > ${VAULT_SERVER_HOME}/init_vars
+
+echo "export VAULT_ADDR=${VAULT_ADDR}" >> ${VAULT_SERVER_HOME}/init_vars
+echo "export VAULT_CACERT=${VAULT_CACERT}" >> ${VAULT_SERVER_HOME}/init_vars
 
 source ${VAULT_SERVER_HOME}/init_vars
 vault unseal -ca-cert=${VAULT_SERVER_HOME}/server.crt -address=${VAULT_ADDR} ${VAULT_UNSEAL_KEY}
