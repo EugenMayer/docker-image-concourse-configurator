c79e51360f751b56f7f53d46c3acc6a1e6a1c457
Eugen Mayer <eugen.mayer@kontextwork.de>
2018-12-02 10:32:07 +0100

    longer cert period

--- bin/vault_client_cert.sh
+++ bin/vault_client_cert.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "creating client certificate"
-openssl req -config /etc/vault_client_cert.conf -x509 -newkey rsa:2048 -keyout ${VAULT_CONCOURSE_CLIENT_HOME}/key.pem -out ${VAULT_CONCOURSE_CLIENT_HOME}/cert.pem -days 365
+openssl req -config /etc/vault_client_cert.conf -x509 -newkey rsa:2048 -keyout ${VAULT_CONCOURSE_CLIENT_HOME}/key.pem -out ${VAULT_CONCOURSE_CLIENT_HOME}/cert.pem -days 1095
